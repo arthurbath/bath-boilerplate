4448b3c4f2967a3e2d5efa89ac80f411b8db2306
Arthur Bath <arthurbath@icloud.com>
2017-05-23 17:04:00 -0700

    Initial project

---
+++ README.md
@@ -0,0 +1,65 @@
+# Bath Boilerplate
+
+### src
+Project source files
+
+### build
+Browser-ready project files
+
+* Processed by Gulp
+
+## Source Control
+
+### .git
+Git repository
+
+### .gitignore
+Git ignore configuration
+
+* Lists filenames and blobs to be ignored by Git
+
+## Dependency Management and Task Running
+
+### .package.json
+NPM manifest
+
+* Defines which packages are required for development
+* Defines scripts for interacting with packages
+
+### node_modules
+Node packages
+
+* Managed by NPM
+
+### gulpfile.js
+Gulp task manifest
+
+* Defines development tasks run by Gulp
+
+## Linting and Formatting
+
+### .editorconfig
+EditorConfig configuration
+
+* Informs text editors to conform to certain syntax rules
+
+### .eslintrc.json
+ESLint configuration
+
+* Defines JavaScript syntax rules that can be enforced by a text editor plugin
+
+### .stylelintrc.json
+Stylelint configuration
+
+* Defines CSS/SCSS syntax rules that can be enforced by a text editor plugin
+* Defines format used for beautifying SCSS source files
+
+### .jsbeautifyrc
+JSBeautify configuration
+
+* Defines format used for beautifying JavaScript source files
+
+### .csscomb.json
+CSSComb configuration
+
+* Defines property ordering inside CSS rulesets that is applied when beautifying SCSS
---
+++ gulpfile.js
@@ -0,0 +1,81 @@
+let del = require('del')
+let gulp = require('gulp')
+let gulpLoadPlugins = require('gulp-load-plugins')
+let $ = gulpLoadPlugins()
+
+// Build tasks
+gulp.task('clearBuild', done => {
+	del(['build/*'])
+	done()
+})
+
+gulp.task('buildHypertext', done => {
+	gulp.src(['src/hypertext/**/*.html', '!**/vendor/**'])
+		.pipe($.htmlmin({ collapseWhitespace: true }))
+		.pipe(gulp.dest('build')) // Copy to build
+	done()
+})
+
+gulp.task('buildScripts', done => {
+	gulp.src(['src/scripts/vendor/*.js', 'src/scripts/**/*.js'])
+		.pipe($.sourcemaps.init()) // Initialize sourcemap
+			.pipe($.concat({ path: 'script.js' })) // Concatenate all
+			.pipe($.babel({
+				presets: ['es2015'],
+				compact: true,
+			})) // Transpile
+			.on('error', console.error.bind(console)) // Catch transpilation error
+			.pipe($.uglify()) // Minify
+		.pipe($.sourcemaps.write('.')) // Write sourcemap
+		.pipe(gulp.dest('build')) // Copy to build
+	done()
+})
+
+gulp.task('buildStyles', done => {
+	gulp.src(['src/styles/**/*.scss', '!**/vendor/**'])
+		.pipe($.sourcemaps.init()) // Initialize sourcemap
+			.pipe($.sass() // Transpile
+			.on('error', $.sass.logError)) // Catch transpilation error
+			.pipe($.autoprefixer()) // Auto-prefix properties for browser support
+			.pipe($.cssnano()) // Minify
+		.pipe($.sourcemaps.write('.')) // Write sourcemap
+		.pipe(gulp.dest('build')) // Copy to build
+	done()
+})
+
+gulp.task('build', gulp.series('clearBuild', 'buildHypertext', 'buildScripts', 'buildStyles'))
+
+gulp.task('watch', done => {
+	gulp.watch('src/hypertext/**/*.html', gulp.series('buildHypertext'))
+	gulp.watch('src/scripts/**/*.js', gulp.series('buildScripts'))
+	gulp.watch('src/styles/**/*.scss', gulp.series('buildStyles'))
+	done()
+})
+
+// Beautification tasks
+gulp.task('beautifyHypertext', done => {
+	gulp.src(['src/hypertext/**/*.html', '!**/vendor/**'])
+		.pipe($.jsbeautifier()) // Beautify src
+		.pipe(gulp.dest('src')) // Replace src
+	done()
+})
+
+gulp.task('beautifyScripts', done => {
+	gulp.src(['src/scripts/**/*.js', '!**/vendor/**'])
+		.pipe($.jsbeautifier()) // Beautify src
+		.pipe(gulp.dest('src')) // Replace src
+	done()
+})
+
+gulp.task('beautifyStyles', done => {
+	gulp.src(['src/styles/**/*.scss', '!**/vendor/**'])
+		.pipe($.csscomb()) // Beautify src
+		.pipe($.stylefmt()) // Beautify src
+		.pipe(gulp.dest('src')) // Replace src
+	done()
+})
+
+gulp.task('beautifySrc', gulp.series('beautifyHypertext', 'beautifyScripts', 'beautifyStyles'))
+
+// Task flows
+gulp.task('default', gulp.series('build', 'watch'))
