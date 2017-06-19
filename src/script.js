d8d5607cdbfe6fee89aa85a477b8c1e0370322d8
Arthur Bath <arthurbath@icloud.com>
2017-06-18 17:10:12 -0700

    Add favicon

--- src/script.js
+++ src/script.js
@@ -1,5 +1,6 @@
 import html from './index.html' // eslint-disable-line no-unused-vars
 import css from './style.scss' // eslint-disable-line no-unused-vars
+import favicon from './favicon.png' // eslint-disable-line no-unused-vars
 
 import $ from 'jquery'
 
